6a8d3e62bc57d1c0d93f626608d55ee899884bcf
Jahanvi Reddy <70678848+jahanvireddy28@users.noreply.github.com>
2021-09-27 02:18:47 +0530

    Add files via upload
    Customer segmentation

---
+++ customer.py
@@ -0,0 +1,11 @@
+# Importing Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Importing the Data for ML Project
+df = pd.read_excel('C:/Users/jahanvi/Documents/Customer_Segmentation/train.xlsx')
+df.shape
+df.head()
+df.dtypes
